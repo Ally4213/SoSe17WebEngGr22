67fa0b610149d8db5b2960b2d940899ab6a17a6d
Astrid Loeffler <astrid@astrid-loeffler.com>
2017-04-19 23:58:08 +0200

    deleted unnecessary data lines

--- UE2/UE2-Angabe/lab2/app/components/controlTypeContinuous.component.js
+++ UE2/UE2-Angabe/lab2/app/components/controlTypeContinuous.component.js
@@ -14,7 +14,6 @@ var ControlTypeContinuousComponent = (function () {
     function ControlTypeContinuousComponent() {
         // lineChart
         this.lineChartData = [
-            //    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
             { data: [28, 48, 40, 19, 0, 27, 27], label: 'Verlauf' },
         ];
         this.lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
--- UE2/UE2-Angabe/lab2/app/components/controlTypeContinuous.component.ts
+++ UE2/UE2-Angabe/lab2/app/components/controlTypeContinuous.component.ts
@@ -21,9 +21,9 @@ export class ControlTypeContinuousComponent {
   
     // lineChart
   public lineChartData:Array<any> = [
-//    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+
     {data: [28, 48, 40, 19, 0, 27, 27], label: 'Verlauf'},
-//    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+
   ];
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
